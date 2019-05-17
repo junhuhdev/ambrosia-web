f98cc9af8bc207531ca4a185539c7adab228b27b
Jun Huh <45692017+junhuhdev@users.noreply.github.com>
2019-05-17 23:41:44 +0200

    added base

---
+++ src/views/Restaurant.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <h1>Restaurants available</h1>
+    </div>
+</template>
+
+<script>
+  import { Component, Vue } from 'vue-property-decorator';
+  import axios from 'axios';
+
+  export default class Restaurant extends Vue {
+
+    data () {
+      return {
+        restaurants: []
+      };
+    }
+
+    created () {
+      axios.get('http://localhost:9000/restaurants')
+        .then(reponse => {
+          this.restaurants = response.data;
+        });
+    }
+
+  }
+</script>
+
+<style scoped>
+
+</style>
