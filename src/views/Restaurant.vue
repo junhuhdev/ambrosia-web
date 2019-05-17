2b39d1bf140c300a6e98682bf55cd5dfa04a6ffd
Jun Huh <45692017+junhuhdev@users.noreply.github.com>
2019-05-18 01:39:56 +0200

    added restaurants

--- src/views/Restaurant.vue
+++ src/views/Restaurant.vue
@@ -1,6 +1,14 @@
 <template>
     <div>
         <h1>Restaurants available</h1>
+        <ul v-if="restaurants">
+            <li v-for="restaurant of restaurants">
+                <p>name: {{restaurant.name}}</p>
+                <p>email: {{restaurant.email}}</p>
+                <p>description: {{restaurant.description}}</p>
+                <p>status: {{restaurant.status}}</p>
+            </li>
+        </ul>
     </div>
 </template>
 
@@ -8,24 +16,30 @@
   import { Component, Vue } from 'vue-property-decorator';
   import axios from 'axios';
 
-  export default class Restaurant extends Vue {
+  export default {
 
     data () {
       return {
-        restaurants: []
+        restaurants: [],
+        errors: []
       };
-    }
+    },
 
     created () {
       axios.get('http://localhost:9000/restaurants')
-        .then(reponse => {
+        .then(response => {
           this.restaurants = response.data;
+        })
+        .catch(e => {
+          this.errors.push(e);
         });
     }
 
-  }
+  };
 </script>
 
 <style scoped>
-
+li {
+    list-style: none;
+}
 </style>
