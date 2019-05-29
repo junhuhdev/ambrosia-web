3439d0bc449bbf02c2c43e01b835ff06e2e4aeb8
Juan Young Huh <juan.huh@evry.com>
2019-05-29 20:59:16 +0200

    added

--- src/views/restaurant/Restaurant.vue
+++ src/views/restaurant/Restaurant.vue
@@ -1,15 +1,13 @@
 <template>
-
     <b-container>
         <h1>Restaurants available</h1>
-
         <b-card-group deck v-if="restaurants">
             <b-row>
                 <b-col cols="4" v-for="restaurant of restaurants" v-bind:key="restaurant.id">
                     <b-link class="custom-card" :to="{name: 'restaurant-detail', params: {id: restaurant.id}}">
                         <b-card :title="restaurant.name" img-src="https://picsum.photos/300/300/?image=41" img-alt="Image" img-top style="margin-top: 2rem;">
-                            <b-card-text>Description: {{restaurant.description}}</b-card-text>
-                            <b-card-text>Status: {{restaurant.status}}</b-card-text>
+                            <b-card-text><b>Description:</b> {{restaurant.description}}</b-card-text>
+                            <b-card-text><b>Status:</b> {{restaurant.status}}</b-card-text>
                             <div slot="footer">
                                 <small class="text-muted">Last updated 3 mins ago</small>
                             </div>
--- src/views/restaurant/RestaurantDetail.vue
+++ src/views/restaurant/RestaurantDetail.vue
@@ -1,5 +1,17 @@
 <template>
-    <h1>Restaurant Details</h1>
+    <b-container>
+        <b-jumbotron>
+            <template slot="header">{{restaurant.name}}</template>
+            <template slot="lead">{{restaurant.description}}</template>
+            <hr class="my-4">
+            <p>
+                It uses utility classes for typography and spacing to space content out within the larger
+                container.
+            </p>
+            <b-button variant="primary" href="#">Do Something</b-button>
+            <b-button variant="success" href="#">Do Something Else</b-button>
+        </b-jumbotron>
+    </b-container>
 </template>
 
 <script>
