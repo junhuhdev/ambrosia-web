397bdd868b0d9f7dcbe60b6d10fab65c0d09a799
Juan Young Huh <juan.huh@evry.com>
2019-05-29 19:13:24 +0200

    added styling for restaurants

--- src/views/Restaurant.vue
+++ src/views/Restaurant.vue
@@ -4,13 +4,17 @@
         <h1>Restaurants available</h1>
 
         <b-card-group deck v-if="restaurants">
-            <b-card v-for="restaurant of restaurants" v-bind:key="restaurant.id" :title="restaurant.name" img-src="https://picsum.photos/300/300/?image=41" img-alt="Image" img-top>
-                <b-card-text>Description: {{restaurant.description}}</b-card-text>
-                <b-card-text>Status: {{restaurant.status}}</b-card-text>
-                <div slot="footer">
-                    <small class="text-muted">Last updated 3 mins ago</small>
-                </div>
-            </b-card>
+            <b-row>
+                <b-col cols="4" v-for="restaurant of restaurants" v-bind:key="restaurant.id">
+                    <b-card :title="restaurant.name" img-src="https://picsum.photos/300/300/?image=41" img-alt="Image" img-top style="margin-top: 2rem;">
+                        <b-card-text>Description: {{restaurant.description}}</b-card-text>
+                        <b-card-text>Status: {{restaurant.status}}</b-card-text>
+                        <div slot="footer">
+                            <small class="text-muted">Last updated 3 mins ago</small>
+                        </div>
+                    </b-card>
+                </b-col>
+            </b-row>
         </b-card-group>
     </b-container>
 </template>
