c5e106f14594686b2374e7046b867f655cf82524
Juan Young Huh <juan.huh@evry.com>
2019-05-29 18:59:46 +0200

    added cards

--- src/views/Restaurant.vue
+++ src/views/Restaurant.vue
@@ -1,36 +1,18 @@
 <template>
-    <div>
+
+    <b-container>
         <h1>Restaurants available</h1>
-        <ul v-if="restaurants">
-            <li v-for="restaurant of restaurants">
-                <p>name: {{restaurant.name}}</p>
-                <p>email: {{restaurant.email}}</p>
-                <p>description: {{restaurant.description}}</p>
-                <p>status: {{restaurant.status}}</p>
-            </li>
-        </ul>
 
-        <b-card
-                title="Card Title"
-                img-src="https://picsum.photos/600/300/?image=25"
-                img-alt="Image"
-                img-top
-                tag="article"
-                style="max-width: 20rem;"
-                class="mb-2"
-        >
-            <b-card-text>
-                Some quick example text to build on the card title and make up the bulk of the card's content.
-            </b-card-text>
-
-            <b-button href="#" variant="primary">Go somewhere</b-button>
-        </b-card>
-
-        <b-jumbotron header="BootstrapVue" lead="Bootstrap 4 Components for Vue.js 2">
-            <p>For more information visit website</p>
-            <b-button variant="primary" href="#">More Info</b-button>
-        </b-jumbotron>
-    </div>
+        <b-card-group deck v-if="restaurants">
+            <b-card v-for="restaurant of restaurants" v-bind:key="restaurant.id" :title="restaurant.name" img-src="https://picsum.photos/300/300/?image=41" img-alt="Image" img-top>
+                <b-card-text>Description: {{restaurant.description}}</b-card-text>
+                <b-card-text>Status: {{restaurant.status}}</b-card-text>
+                <div slot="footer">
+                    <small class="text-muted">Last updated 3 mins ago</small>
+                </div>
+            </b-card>
+        </b-card-group>
+    </b-container>
 </template>
 
 <script>
