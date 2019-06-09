4f11efd92b4986edec208d03affb4ccd3a27e139
Juan Young Huh <juan.huh@evry.com>
2019-06-09 14:41:28 +0200

    fixat sök styling

--- src/views/home/HomePage.vue
+++ src/views/home/HomePage.vue
@@ -1,20 +1,37 @@
 <template>
   <b-container>
-    <b-row>
-      <h1>Välkommen till Ambrosia</h1>
-    </b-row>
+    <div class="search-bar">
+      <h1 style="margin-bottom: 30px">Vad är jag sugen på?</h1>
+      <vue-bootstrap-typeahead placeholder="Restaurang, meny, mat..." v-model="query" :data="['Canada', 'USA', 'Mexico']">
+        <template slot="append">
+          <b-button @click="search" variant="primary">Sök</b-button>
+        </template>
+      </vue-bootstrap-typeahead>
+    </div>
   </b-container>
 </template>
 <script lang="ts">
   import Vue from 'vue';
+  import VueBootstrapTypeahead from 'vue-bootstrap-typeahead';
 
   export default Vue.extend({
+    components: {VueBootstrapTypeahead},
+
     data() {
       return {
-        title: 'Welcome to ambrosia'
+        query: ''
       };
+    },
+
+    methods: {
+      search() {
+
+      }
     }
   });
 </script>
 <style scoped>
+  .search-bar {
+    margin-top: 4em;
+  }
 </style>
