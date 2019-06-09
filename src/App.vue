0549684ffd000c1733727cbebb61d9571b858694
Juan Young Huh <juan.huh@evry.com>
2019-06-09 18:08:04 +0200

    reorganized navbar header

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,18 @@
 <template>
   <div id="app">
-    <b-navbar toggleable="lg" type="dark" variant="info">
-      <b-navbar-brand to="/">Ambrosia</b-navbar-brand>
-      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-      <b-collapse id="nav-collapse" is-nav>
-        <b-navbar-nav>
-          <b-nav-item :to="{path: '/login'}">Login</b-nav-item>
-          <b-nav-item :to="{path: '/register'}">Register</b-nav-item>
-          <b-nav-item :to="{path: '/restaurant'}">Restaurant</b-nav-item>
-          <b-nav-item :to="{path: '/meal'}">Meal</b-nav-item>
-          <b-nav-item :to="{path: '/order'}">Order</b-nav-item>
-          <b-nav-item-dropdown text="Admin">
-            <b-dropdown-item :to="{path: '/restaurant-register-form'}">Create Restaurant</b-dropdown-item>
-            <b-dropdown-item :to="{path: '/menu-register-form'}">Create Menu</b-dropdown-item>
-            <b-dropdown-item :to="{path: '/meal-register-form'}">Create Meal</b-dropdown-item>
-          </b-nav-item-dropdown>
-        </b-navbar-nav>
-        <!-- Right aligned nav items -->
-        <b-navbar-nav class="ml-auto">
-          <b-nav-item-dropdown right>
-            <!-- Using 'button-content' slot -->
-            <template slot="button-content"><em>User</em></template>
-            <b-dropdown-item href="#">Profile</b-dropdown-item>
-            <b-dropdown-item href="#">Sign Out</b-dropdown-item>
-          </b-nav-item-dropdown>
-          <CartHeader/>
-        </b-navbar-nav>
-      </b-collapse>
-    </b-navbar>
+    <NavbarHeader/>
     <br>
     <router-view/>
   </div>
 </template>
 <script lang="ts">
   import Vue from 'vue';
-  import CartHeader from '@/views/cart/CartHeader.vue';
+  import NavbarHeader from './views/main/NavbarHeader.vue';
 
   export default Vue.extend({
-    components: {CartHeader}
+    components: {
+      NavbarHeader
+    }
   });
 </script>
 <style>
---
+++ src/views/main/NavbarHeader.vue
@@ -0,0 +1,43 @@
+<template>
+  <b-navbar toggleable="lg" type="dark" variant="info">
+    <b-navbar-brand to="/">Ambrosia</b-navbar-brand>
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav>
+        <b-nav-item :to="{path: '/login'}">Login</b-nav-item>
+        <b-nav-item :to="{path: '/register'}">Register</b-nav-item>
+        <b-nav-item :to="{path: '/restaurant'}">Restaurant</b-nav-item>
+        <b-nav-item :to="{path: '/meal'}">Meal</b-nav-item>
+        <b-nav-item :to="{path: '/order'}">Order</b-nav-item>
+        <b-nav-item-dropdown text="Admin">
+          <b-dropdown-item :to="{path: '/restaurant-register-form'}">Create Restaurant</b-dropdown-item>
+          <b-dropdown-item :to="{path: '/menu-register-form'}">Create Menu</b-dropdown-item>
+          <b-dropdown-item :to="{path: '/meal-register-form'}">Create Meal</b-dropdown-item>
+        </b-nav-item-dropdown>
+      </b-navbar-nav>
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item-dropdown right>
+          <!-- Using 'button-content' slot -->
+          <template slot="button-content"><em>User</em></template>
+          <b-dropdown-item href="#">Profile</b-dropdown-item>
+          <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+        </b-nav-item-dropdown>
+        <CartHeader/>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</template>
+<script lang="ts">
+  import Vue from 'vue';
+  import CartHeader from '@/views/cart/CartHeader.vue';
+
+  export default Vue.extend({
+    components: {
+      CartHeader
+    }
+
+  });
+</script>
+<style scoped>
+</style>
