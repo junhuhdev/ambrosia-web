d7ff8bff42b6da9fa897a59191653eb69f4e7336
Juan Young Huh <juan.huh@evry.com>
2019-09-14 23:03:41 +0200

    x

--- src/components/NavbarHeader.vue
+++ src/components/NavbarHeader.vue
@@ -11,13 +11,41 @@
       <v-toolbar-items class="hidden-sm-and-down">
         <v-btn text v-for="item in items" :key="item.title" :to="item.link">{{item.title}}</v-btn>
       </v-toolbar-items>
+      <CartMenu></CartMenu>
+<!--      <v-dialog v-model="dialog" width="600px">-->
+<!--        <template v-slot:activator="{ on }">-->
+<!--          <v-toolbar-items>-->
+<!--            <v-btn text icon v-on="on">-->
+<!--              <v-icon>mdi-cart</v-icon>-->
+<!--            </v-btn>-->
+<!--          </v-toolbar-items>-->
+<!--        </template>-->
+<!--        <v-card>-->
+<!--          <v-card-title>-->
+<!--            <span class="headline">Use Google's location service?</span>-->
+<!--          </v-card-title>-->
+<!--          <v-card-text>-->
+<!--            Lasdsad-->
+<!--          </v-card-text>-->
+<!--          <v-card-actions>-->
+<!--            <div class="flex-grow-1"></div>-->
+<!--            <v-btn color="green darken-1" text @click="dialog = false">Disagree</v-btn>-->
+<!--            <v-btn color="green darken-1" text @click="dialog = false">Agree</v-btn>-->
+<!--          </v-card-actions>-->
+<!--        </v-card>-->
+<!--      </v-dialog>-->
     </v-app-bar>
   </div>
 </template>
 <script lang="ts">
   import Vue from 'vue';
+  import CartMenu from '@/components/carts/CartMenu.vue';
 
   export default Vue.extend({
+    components: {
+      CartMenu
+    },
+
     data: () => ({
       drawer: null,
       items: [
@@ -25,7 +53,6 @@
         {title: 'Beställ catering', link: '/restaurant'},
         {title: 'Bli leverantör', link: '/catering'},
         {title: 'Kontakta oss', link: '/catering'},
-        {title: 'Logga in', link: '/catering'},
       ]
     })
   });
---
+++ src/components/carts/CartMenu.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="text-center">
+    <v-menu
+        v-model="menu"
+        :close-on-content-click="false"
+        :nudge-width="200"
+        offset-y
+    >
+      <template v-slot:activator="{ on }">
+        <v-btn
+            text
+            icon
+            v-on="on"
+        >
+          <v-icon>mdi-cart</v-icon>
+        </v-btn>
+      </template>
+      <v-card>
+        <v-list>
+          <v-list-item>
+            <v-list-item-avatar>
+            </v-list-item-avatar>
+            <v-list-item-content>
+            </v-list-item-content>
+            <v-list-item-action>
+              <v-btn rounded color="primary">
+                Till varukorgen
+              </v-btn>
+            </v-list-item-action>
+          </v-list-item>
+        </v-list>
+        <v-divider></v-divider>
+        <v-list>
+          <v-list-item>
+            <v-list-item-action>
+              <v-switch v-model="message" color="purple"></v-switch>
+            </v-list-item-action>
+            <v-list-item-title>Enable messages</v-list-item-title>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-action>
+              <v-switch v-model="hints" color="purple"></v-switch>
+            </v-list-item-action>
+            <v-list-item-title>Enable hints</v-list-item-title>
+          </v-list-item>
+        </v-list>
+        <v-card-actions>
+          <div class="flex-grow-1"></div>
+          <v-btn text @click="menu = false">Cancel</v-btn>
+          <v-btn color="primary" text @click="menu = false">Save</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-menu>
+  </div>
+</template>
+<script lang="ts">
+  import Vue from 'vue';
+
+  export default Vue.extend({
+
+    data: () => ({
+      fav: true,
+      menu: false,
+      message: false,
+      hints: true,
+    }),
+
+  });
+</script>
+<style scoped>
+</style>
