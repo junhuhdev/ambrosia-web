306b5a19acca1c69aa8007ba349317db7695f6dc
Juan Young Huh <juan.huh@evry.com>
2019-09-14 23:44:40 +0200

    x

--- src/components/carts/CartMenuList.vue
+++ src/components/carts/CartMenuList.vue
@@ -8,7 +8,22 @@
         <v-list-item-content>
           <v-list-item-title v-html="item.title"></v-list-item-title>
           <v-list-item-subtitle v-html="item.subtitle"></v-list-item-subtitle>
+          <v-btn-toggle v-model="toggle" rounded>
+            <v-btn>
+              <v-icon>mdi-minus</v-icon>
+            </v-btn>
+            <v-btn>
+              <v-icon>mdi-format-align-center</v-icon>
+            </v-btn>
+            <v-btn fab dark color="red" icon>
+              <v-icon>mdi-plus</v-icon>
+            </v-btn>
+          </v-btn-toggle>
         </v-list-item-content>
+        <v-list-item-action>
+          <v-icon>mdi-trash-can-outline</v-icon>
+          <v-list-item-action-text v-text="item.price"></v-list-item-action-text>
+        </v-list-item-action>
       </v-list-item>
     </v-list-item-group>
   </v-list>
@@ -18,21 +33,25 @@
 
   export default Vue.extend({
     data: () => ({
+      toggle: null,
       items: [
         {
           img: 'https://d2rfo6yapuixuu.cloudfront.net/h4d/h03/9374221860894/2359715300003_1550505666334_master_axfood_300',
           title: 'Kyckling hel färsk',
-          subtitle: 'GARANT, ca 1.3 kg'
+          subtitle: 'GARANT, ca 1.3 kg',
+          price: '39,00 kr'
         },
         {
           img: 'https://d2rfo6yapuixuu.cloudfront.net/h04/h23/9086779097118/07340083463143.jpg_1520719678758_master_axfood_300',
           title: 'Bacon skivat',
-          subtitle: 'GARANT EKO, 120 g'
+          subtitle: 'GARANT EKO, 120 g',
+          price: '19,95 kr'
         },
         {
           img: 'https://d2rfo6yapuixuu.cloudfront.net/he1/h66/8881178214430/07350002402955.jpg_1480078266950_master_axfood_300',
           title: 'Kikärtor ekologiska',
-          subtitle: 'ZETA, 120 g'
+          subtitle: 'ZETA, 120 g',
+          price: '13,95 kr'
         }
       ]
     })
