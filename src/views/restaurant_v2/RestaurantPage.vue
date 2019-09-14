77f11d0b541ea7e21975cef30078891f827f5147
Juan Young Huh <juan.huh@evry.com>
2019-09-14 17:45:36 +0200

    x

--- src/views/restaurant_v2/RestaurantPage.vue
+++ src/views/restaurant_v2/RestaurantPage.vue
@@ -1,5 +1,63 @@
 <template>
   <v-container>
+    <v-card class="mx-auto" max-width="1200">
+      <v-toolbar dense flat color="deep-purple accent-4" dark>
+        <v-btn icon>
+          <v-icon>mdi-close</v-icon>
+        </v-btn>
+        <v-toolbar-title>Valda filter</v-toolbar-title>
+      </v-toolbar>
+      <v-card-text>
+        <v-row>
+          <v-col cols="6">
+            <h2 class="title mb-2">Menyer</h2>
+            <v-chip-group column multiple>
+              <v-chip filter outlined>Lunch</v-chip>
+              <v-chip filter outlined>Middag</v-chip>
+              <v-chip filter outlined>Frukost</v-chip>
+              <v-chip filter outlined>Fika</v-chip>
+              <v-chip filter outlined>Plockmat</v-chip>
+              <v-chip filter outlined>Julmeny</v-chip>
+              <v-chip filter outlined>Tillbehör</v-chip>
+              <v-chip filter outlined>Drycker</v-chip>
+              <v-chip filter outlined>Food truck</v-chip>
+              <v-chip filter outlined>Bar</v-chip>
+            </v-chip-group>
+          </v-col>
+          <v-col cols="6">
+            <h2 class="title mb-2">Vad</h2>
+            <v-chip-group column multiple>
+              <v-chip filter outlined>Sallader</v-chip>
+              <v-chip filter outlined>Buffé</v-chip>
+              <v-chip filter outlined>Dryck</v-chip>
+              <v-chip filter outlined>Wraps</v-chip>
+              <v-chip filter outlined>Smörgåsar</v-chip>
+              <v-chip filter outlined>Fikabröd</v-chip>
+              <v-chip filter outlined>Huvudrätter</v-chip>
+              <v-chip filter outlined>Tillbehör</v-chip>
+              <v-chip filter outlined>Smårätter</v-chip>
+              <v-chip filter outlined>Tårtor</v-chip>
+            </v-chip-group>
+          </v-col>
+          <v-col cols="6">
+            <h2 class="title mb-2">Innehåll</h2>
+            <v-chip-group column multiple>
+              <v-chip filter outlined>Vegetariskt</v-chip>
+              <v-chip filter outlined>Vegansk</v-chip>
+              <v-chip filter outlined>Glutenfri</v-chip>
+              <v-chip filter outlined>Laktosfri</v-chip>
+              <v-chip filter outlined>Mjölkproteinfri</v-chip>
+            </v-chip-group>
+          </v-col>
+          <v-col cols="6">
+            <h2 class="title mb-2">Hållbart val</h2>
+            <v-chip-group column multiple>
+              <v-chip filter outlined>Plastfri</v-chip>
+            </v-chip-group>
+          </v-col>
+        </v-row>
+      </v-card-text>
+    </v-card>
     <v-row no-gutters>
       <v-col cols="12" sm="3" v-for="restaurant in restaurants" v-bind:key="restaurant.id">
         <RestaurantCard
