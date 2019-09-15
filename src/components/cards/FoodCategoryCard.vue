93a62ef29f3ed9ab50cd96fc12942509a316de7d
Juan Young Huh <juan.huh@evry.com>
2019-09-15 11:28:22 +0200

    x

--- src/components/cards/FoodCategoryCard.vue
+++ src/components/cards/FoodCategoryCard.vue
@@ -1,13 +1,29 @@
 <template>
-  <v-card class="elevation-12">
-    <v-img
-        class="white--text"
-        height="300px"
-        :src="imgSource"
-    >
-      <v-card-title class="justify-center fill-height">{{title}}</v-card-title>
-    </v-img>
-  </v-card>
+  <v-hover>
+    <template v-slot:default="{ hover }">
+      <v-card
+          :elevation="hover ? 12 : 2"
+          class="mx-auto"
+      >
+        <v-img
+            class="white--text"
+            height="300px"
+            :src="imgSource"
+        >
+          <v-card-title class="justify-center fill-height">{{title}}</v-card-title>
+        </v-img>
+        <v-fade-transition>
+          <v-overlay
+              v-if="hover"
+              absolute
+              color="#036358"
+          >
+            <v-btn>Se vårt {{title}} utbud</v-btn>
+          </v-overlay>
+        </v-fade-transition>
+      </v-card>
+    </template>
+  </v-hover>
 </template>
 <script lang="ts">
   import Vue from 'vue';
@@ -16,7 +32,11 @@
     props: {
       imgSource: String,
       title: String
-    }
+    },
+
+    data: () => ({
+      hover: false
+    })
 
   });
 </script>
--- src/views/restaurant_v2/RestaurantCard.vue
+++ src/views/restaurant_v2/RestaurantCard.vue
@@ -1,43 +1,56 @@
 <template>
-  <v-card
-      :loading="loading"
-      class="mx-auto my-12 elevation-12"
-      max-width="410"
-  >
-    <v-img height="250" src="https://cdn.vuetifyjs.com/images/cards/cooking.png"></v-img>
-    <v-card-title>{{title}}</v-card-title>
-    <v-card-text>
-      <v-row align="center">
-        <v-rating
-            :value="4.5"
-            color="amber"
-            half-increments
-            dense
-            size="14"
-            readonly
-        ></v-rating>
-        <div class="grey--text ml-4">4.5 (413)</div>
-      </v-row>
-      <div class="my-4 subtitle-1 black--text">
-        $ • Italian, Cafe
-      </div>
-      <div>{{description}}</div>
-    </v-card-text>
-    <v-divider class="mx-4"></v-divider>
-    <v-card-text>
-      <div class="title text--primary">Kvällens tillgänglighet</div>
-      <v-chip-group
-          v-model="selection"
-          active-class="deep-purple accent-4 white--text"
-          column
+  <v-hover>
+    <template v-slot:default="{hover}">
+      <v-card
+          :loading="loading"
+          class="mx-auto my-12 elevation-12"
+          max-width="410"
       >
-        <v-chip>5:30PM</v-chip>
-        <v-chip>7:30PM</v-chip>
-        <v-chip>8:00PM</v-chip>
-        <v-chip>9:00PM</v-chip>
-      </v-chip-group>
-    </v-card-text>
-  </v-card>
+        <v-img height="250" src="https://cdn.vuetifyjs.com/images/cards/cooking.png"></v-img>
+        <v-card-title>{{title}}</v-card-title>
+        <v-card-text>
+          <v-row align="center">
+            <v-rating
+                :value="4.5"
+                color="amber"
+                half-increments
+                dense
+                size="14"
+                readonly
+            ></v-rating>
+            <div class="grey--text ml-4">4.5 (413)</div>
+          </v-row>
+          <div class="my-4 subtitle-1 black--text">
+            $ • Italian, Cafe
+          </div>
+          <div>{{description}}</div>
+        </v-card-text>
+        <v-divider class="mx-4"></v-divider>
+        <v-card-text>
+          <div class="title text--primary">Kvällens tillgänglighet</div>
+          <v-chip-group
+              v-model="selection"
+              active-class="deep-purple accent-4 white--text"
+              column
+          >
+            <v-chip>5:30PM</v-chip>
+            <v-chip>7:30PM</v-chip>
+            <v-chip>8:00PM</v-chip>
+            <v-chip>9:00PM</v-chip>
+          </v-chip-group>
+        </v-card-text>
+        <v-fade-transition>
+          <v-overlay
+              v-if="hover"
+              absolute
+              color="#036358"
+          >
+            <v-btn>Se meny</v-btn>
+          </v-overlay>
+        </v-fade-transition>
+      </v-card>
+    </template>
+  </v-hover>
 </template>
 <script lang="ts">
   import Vue from 'vue';
@@ -52,6 +65,7 @@
     data: () => ({
       loading: false,
       selection: 1,
+      hover: false,
     }),
 
     methods: {
