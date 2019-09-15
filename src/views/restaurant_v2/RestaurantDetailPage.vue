e6cf1cb724877e59a383fb9445267cf4a995aca1
Juan Young Huh <juan.huh@evry.com>
2019-09-15 12:58:57 +0200

    x

--- src/views/restaurant_v2/RestaurantDetailPage.vue
+++ src/views/restaurant_v2/RestaurantDetailPage.vue
@@ -8,33 +8,24 @@
           </v-row>
         </v-parallax>
       </v-col>
-      <v-col md="8" offset-md="2">
+      <v-col v-for="item in items" :key="item.category" md="6">
         <v-card>
           <v-toolbar color="pink lighten-1" dark>
             <v-row align="center" justify="center">
-              <v-toolbar-title class="text-center">Frukost</v-toolbar-title>
+              <v-toolbar-title class="text-center">{{item.category}}</v-toolbar-title>
             </v-row>
           </v-toolbar>
           <v-list subheader>
             <v-list-item-group>
-              <v-list-item v-for="item in items" :key="item.title">
+              <v-list-item v-for="food in item.foods" :key="food.title">
                 <v-list-item-avatar size="200" horizontal>
-                  <v-img :src="item.img"></v-img>
+                  <v-img :src="food.img"></v-img>
                 </v-list-item-avatar>
                 <v-list-item-content>
-                  <v-list-item-title v-html="item.title"></v-list-item-title>
-                  <v-list-item-subtitle v-html="item.subtitle"></v-list-item-subtitle>
+                  <v-list-item-title v-html="food.title"></v-list-item-title>
+                  <v-list-item-subtitle v-html="food.subtitle"></v-list-item-subtitle>
                 </v-list-item-content>
                 <v-list-item-action>
-                  <v-btn-toggle class="btn-group-sm">
-                    <v-btn x-small fab color="red" v-on:click="item.count -= 1">
-                      <v-icon color="white">mdi-minus</v-icon>
-                    </v-btn>
-                    <v-btn x-small fab color="white" class="subtitle-2">{{item.count}}</v-btn>
-                    <v-btn x-small fab color="red">
-                      <v-icon color="white">mdi-plus</v-icon>
-                    </v-btn>
-                  </v-btn-toggle>
                 </v-list-item-action>
               </v-list-item>
             </v-list-item-group>
@@ -50,12 +41,40 @@
   export default Vue.extend({
     data: () => ({
       items: [
-        {title: 'Lunch Biff Rogan Josh', subtitle: 'Biff med färsk tomat, en blandning av pepparnejlikor, och rostade korianderfrön.', count: 0, price: 95.50, img: 'https://caterbee.blob.core.windows.net/caterbee/cf/350x233/bythebakery-catering.jpg'},
-        {title: 'Lunch Biff Rogan Josh', subtitle: 'Biff med färsk tomat, en blandning av pepparnejlikor, och rostade korianderfrön.', count: 0, price: 95.50, img: 'https://caterbee.blob.core.windows.net/caterbee/cf/350x233/panini-catering.jpg'},
-        {title: 'Lunch Biff Rogan Josh', subtitle: 'Biff med färsk tomat, en blandning av pepparnejlikor, och rostade korianderfrön.', count: 0, price: 95.50, img: 'https://caterbee.blob.core.windows.net/caterbee/cf/350x233/thelins-catering.jpg'},
-        {title: 'Lunch Biff Rogan Josh', subtitle: 'Biff med färsk tomat, en blandning av pepparnejlikor, och rostade korianderfrön.', count: 0, price: 95.50, img: 'https://caterbee.blob.core.windows.net/caterbee/cf/350x233/pizza-isabellesmarket.jpg'},
-        {title: 'Lunch Biff Rogan Josh', subtitle: 'Biff med färsk tomat, en blandning av pepparnejlikor, och rostade korianderfrön.', count: 0, price: 95.50, img: 'https://caterbee.blob.core.windows.net/caterbee/cf/350x233/drmat-catering.jpg'},
-        {title: 'Lunch Biff Rogan Josh', subtitle: 'Biff med färsk tomat, en blandning av pepparnejlikor, och rostade korianderfrön.', count: 0, price: 95.50, img: 'https://caterbee.blob.core.windows.net/caterbee/cf/350x233/matsallskapet-catering.jpg'},
+        {
+          category: 'Frukost',
+          foods: [
+            {title: 'Lunch Biff Rogan Josh', subtitle: 'Biff med färsk tomat, en blandning av pepparnejlikor, och rostade korianderfrön.', count: 0, price: 95.50, img: 'https://caterbee.blob.core.windows.net/caterbee/cf/350x233/bythebakery-catering.jpg'},
+            {title: 'Lunch Biff Rogan Josh', subtitle: 'Biff med färsk tomat, en blandning av pepparnejlikor, och rostade korianderfrön.', count: 0, price: 95.50, img: 'https://caterbee.blob.core.windows.net/caterbee/cf/350x233/panini-catering.jpg'},
+            {title: 'Lunch Biff Rogan Josh', subtitle: 'Biff med färsk tomat, en blandning av pepparnejlikor, och rostade korianderfrön.', count: 0, price: 95.50, img: 'https://caterbee.blob.core.windows.net/caterbee/cf/350x233/thelins-catering.jpg'},
+            {title: 'Lunch Biff Rogan Josh', subtitle: 'Biff med färsk tomat, en blandning av pepparnejlikor, och rostade korianderfrön.', count: 0, price: 95.50, img: 'https://caterbee.blob.core.windows.net/caterbee/cf/350x233/pizza-isabellesmarket.jpg'},
+            {title: 'Lunch Biff Rogan Josh', subtitle: 'Biff med färsk tomat, en blandning av pepparnejlikor, och rostade korianderfrön.', count: 0, price: 95.50, img: 'https://caterbee.blob.core.windows.net/caterbee/cf/350x233/drmat-catering.jpg'},
+            {title: 'Lunch Biff Rogan Josh', subtitle: 'Biff med färsk tomat, en blandning av pepparnejlikor, och rostade korianderfrön.', count: 0, price: 95.50, img: 'https://caterbee.blob.core.windows.net/caterbee/cf/350x233/matsallskapet-catering.jpg'},
+          ]
+        },
+        {
+          category: 'Lunch',
+          foods: [
+            {title: 'Lunch Biff Rogan Josh', subtitle: 'Biff med färsk tomat, en blandning av pepparnejlikor, och rostade korianderfrön.', count: 0, price: 95.50, img: 'https://caterbee.blob.core.windows.net/caterbee/cf/350x233/bythebakery-catering.jpg'},
+            {title: 'Lunch Biff Rogan Josh', subtitle: 'Biff med färsk tomat, en blandning av pepparnejlikor, och rostade korianderfrön.', count: 0, price: 95.50, img: 'https://caterbee.blob.core.windows.net/caterbee/cf/350x233/panini-catering.jpg'},
+            {title: 'Lunch Biff Rogan Josh', subtitle: 'Biff med färsk tomat, en blandning av pepparnejlikor, och rostade korianderfrön.', count: 0, price: 95.50, img: 'https://caterbee.blob.core.windows.net/caterbee/cf/350x233/thelins-catering.jpg'},
+            {title: 'Lunch Biff Rogan Josh', subtitle: 'Biff med färsk tomat, en blandning av pepparnejlikor, och rostade korianderfrön.', count: 0, price: 95.50, img: 'https://caterbee.blob.core.windows.net/caterbee/cf/350x233/pizza-isabellesmarket.jpg'},
+            {title: 'Lunch Biff Rogan Josh', subtitle: 'Biff med färsk tomat, en blandning av pepparnejlikor, och rostade korianderfrön.', count: 0, price: 95.50, img: 'https://caterbee.blob.core.windows.net/caterbee/cf/350x233/drmat-catering.jpg'},
+            {title: 'Lunch Biff Rogan Josh', subtitle: 'Biff med färsk tomat, en blandning av pepparnejlikor, och rostade korianderfrön.', count: 0, price: 95.50, img: 'https://caterbee.blob.core.windows.net/caterbee/cf/350x233/matsallskapet-catering.jpg'},
+          ]
+        },
+        {
+          category: 'Middag',
+          foods: [
+            {title: 'Lunch Biff Rogan Josh', subtitle: 'Biff med färsk tomat, en blandning av pepparnejlikor, och rostade korianderfrön.', count: 0, price: 95.50, img: 'https://caterbee.blob.core.windows.net/caterbee/cf/350x233/bythebakery-catering.jpg'},
+            {title: 'Lunch Biff Rogan Josh', subtitle: 'Biff med färsk tomat, en blandning av pepparnejlikor, och rostade korianderfrön.', count: 0, price: 95.50, img: 'https://caterbee.blob.core.windows.net/caterbee/cf/350x233/panini-catering.jpg'},
+            {title: 'Lunch Biff Rogan Josh', subtitle: 'Biff med färsk tomat, en blandning av pepparnejlikor, och rostade korianderfrön.', count: 0, price: 95.50, img: 'https://caterbee.blob.core.windows.net/caterbee/cf/350x233/thelins-catering.jpg'},
+            {title: 'Lunch Biff Rogan Josh', subtitle: 'Biff med färsk tomat, en blandning av pepparnejlikor, och rostade korianderfrön.', count: 0, price: 95.50, img: 'https://caterbee.blob.core.windows.net/caterbee/cf/350x233/pizza-isabellesmarket.jpg'},
+            {title: 'Lunch Biff Rogan Josh', subtitle: 'Biff med färsk tomat, en blandning av pepparnejlikor, och rostade korianderfrön.', count: 0, price: 95.50, img: 'https://caterbee.blob.core.windows.net/caterbee/cf/350x233/drmat-catering.jpg'},
+            {title: 'Lunch Biff Rogan Josh', subtitle: 'Biff med färsk tomat, en blandning av pepparnejlikor, och rostade korianderfrön.', count: 0, price: 95.50, img: 'https://caterbee.blob.core.windows.net/caterbee/cf/350x233/matsallskapet-catering.jpg'},
+          ]
+        },
+
       ]
     })
 
